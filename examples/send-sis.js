4a3eec813a40ad366a84a8a6fb82702777dc41f3
Carlos Saito <carlos@saito.style>
2021-11-29 14:37:44 +0100

    Fix version for form-data-encoder

--- examples/send-sis.js
+++ examples/send-sis.js
@@ -4,12 +4,38 @@ const canvasApiUrl = process.env.CANVAS_API_URL;
 const canvasApiToken = process.env.CANVAS_API_TOKEN;
 const Canvas = require("../dist/index").default;
 const path = require("path");
+const fs = require("fs");
+const os = require("os");
+const csv = require("fast-csv");
+
+const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), "canvas-api-"));
 
 async function start() {
   console.log("Performing a SIS Import");
   const canvas = new Canvas(canvasApiUrl, canvasApiToken);
+  const filePath = path.join(tmpDir, "test.csv");
 
-  const filePath = path.join(__dirname, "test.csv");
+  console.log(filePath);
+
+  const writer = fs.createWriteStream(filePath);
+  const serializer = csv.format({ headers: true });
+
+  serializer.pipe(writer);
+
+  for (let i = 0; i < 100; i++) {
+    serializer.write({
+      user_id: i,
+      section_id: i,
+      status: "active",
+    });
+  }
+
+  serializer.end();
+
+  await new Promise((resolve, reject) => {
+    writer.on("finish", resolve);
+    writer.on("error", reject);
+  });
 
   try {
     const { body } = await canvas.sisImport(filePath);
