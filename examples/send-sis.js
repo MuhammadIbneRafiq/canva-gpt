1498da44a658c2661de6fc57301484c1c1013e48
Carlos Saito <carlos@saito.style>
2021-11-29 12:32:18 +0100

    Add example that fails

---
+++ examples/send-sis.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+require("dotenv").config();
+const canvasApiUrl = process.env.CANVAS_API_URL;
+const canvasApiToken = process.env.CANVAS_API_TOKEN;
+const Canvas = require("../dist/index").default;
+const path = require("path");
+
+async function start() {
+  console.log("Performing a SIS Import");
+  const canvas = new Canvas(canvasApiUrl, canvasApiToken);
+
+  const filePath = path.join(__dirname, "test.csv");
+  const { body } = await canvas.sisImport(filePath);
+  console.log(body);
+}
+
+start();
